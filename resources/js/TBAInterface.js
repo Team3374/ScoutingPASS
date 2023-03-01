625497dfa9a73e3492b717a37ee954605468c6df
BohdanIB <95309441+BohdanIB@users.noreply.github.com>
2023-02-28 17:41:51 -0700

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "9XBqx2EpiBWlJmd7JIrrcP0mLRnQj2qm3XvmSTLlNZcqD9Tp2wGt3ZgNkQscUhGv";
 /**
  * Get list of teams in event
  *
